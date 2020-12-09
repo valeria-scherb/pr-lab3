9ffd67df161db6f029d675324de3e76a7d21c997
Valeria <valeriascherb@gmail.com>
2020-12-09 19:35:07 +0200

    More information about training results

--- main.py
+++ main.py
@@ -38,3 +38,16 @@ av_cr_1 = sum(cl_res_1) / len(cl_res_1)
 print('[Training] Average class for zeros:', av_cr_0)
 print('[Training] Average class for ones: ', av_cr_1)
 
+class_0 = round(av_cr_0)
+class_1 = round(av_cr_1)
+
+print('EM chose class', class_0, 'for zeros and', class_1, 'for ones')
+print('The result is more biased towards ' + ('zeros' if
+      (abs(av_cr_0 - class_0) < abs(av_cr_1 - class_1)) else 'ones'))
+
+print('[Training] Mistakes on zeros:',
+      sum([1 if cl_res_0[i] != class_0 else 0 for i in range(0, len(cl_res_0))]),
+      '/', len(cl_res_0))
+print('[Training] Mistakes on ones:',
+      sum([1 if cl_res_1[i] != class_1 else 0 for i in range(0, len(cl_res_0))]),
+      '/', len(cl_res_1))
