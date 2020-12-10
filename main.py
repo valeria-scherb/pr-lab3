9ed759e8f624425d57d692c7dcd89016905e0f08
Valeria <valeriascherb@gmail.com>
2020-12-10 22:59:11 +0200

    Move duplicate code to function in main

--- main.py
+++ main.py
@@ -30,63 +30,29 @@ em = EM(nr.im_size, 2)
 em.load_train(data)
 em.run(iterations)
 
-cl_res_0 = [em.classify(x) for x in train[0]]
-cl_res_1 = [em.classify(x) for x in train[1]]
-av_cr_0 = sum(cl_res_0) / len(cl_res_0)
-av_cr_1 = sum(cl_res_1) / len(cl_res_1)
 
-print('[Training] Average class for zeros:', av_cr_0)
-print('[Training] Average class for ones: ', av_cr_1)
+def analyze_set(items, descr):
+    cl_res_0 = [em.classify(x) for x in items[0]]
+    cl_res_1 = [em.classify(x) for x in items[1]]
+    av_cr_0 = sum(cl_res_0) / len(cl_res_0)
+    av_cr_1 = sum(cl_res_1) / len(cl_res_1)
+    print('')
+    print('['+descr+'] Average class for zeros:', av_cr_0)
+    print('['+descr+'] Average class for ones: ', av_cr_1)
+    class_0 = round(av_cr_0)
+    class_1 = round(av_cr_1)
+    print('EM chose class', class_0, 'for zeros and', class_1, 'for ones')
+    print('The result is more biased towards ' + ('zeros' if
+        (abs(av_cr_0 - class_0) < abs(av_cr_1 - class_1)) else 'ones'))
+    mistakes_0 = sum([1 if cl_res_0[i] != class_0 else 0 for i in range(0, len(cl_res_0))])
+    mistakes_1 = sum([1 if cl_res_1[i] != class_1 else 0 for i in range(0, len(cl_res_0))])
+    print('['+descr+'] Mistakes on zeros:', mistakes_0, '/', len(cl_res_0),
+          '(' + str(100 * mistakes_0 / len(cl_res_0)) + '%)')
+    print('['+descr+'] Mistakes on ones:', mistakes_1, '/', len(cl_res_1),
+          '(' + str(100 * mistakes_1 / len(cl_res_1)) + '%)')
 
-class_0 = round(av_cr_0)
-class_1 = round(av_cr_1)
 
-print('EM chose class', class_0, 'for zeros and', class_1, 'for ones')
-print('The result is more biased towards ' + ('zeros' if
-      (abs(av_cr_0 - class_0) < abs(av_cr_1 - class_1)) else 'ones'))
-
-mistakes_0 = sum([1 if cl_res_0[i] != class_0 else 0 for i in range(0, len(cl_res_0))])
-mistakes_1 = sum([1 if cl_res_1[i] != class_1 else 0 for i in range(0, len(cl_res_0))])
-print('[Training] Mistakes on zeros:', mistakes_0, '/', len(cl_res_0),
-      '(' + str(100 * mistakes_0 / len(cl_res_0)) + '%)')
-print('[Training] Mistakes on ones:', mistakes_1, '/', len(cl_res_1),
-      '(' + str(100 * mistakes_1 / len(cl_res_1)) + '%)')
-
-cl_res_0 = [em.classify(x) for x in test1[0]]
-cl_res_1 = [em.classify(x) for x in test1[1]]
-av_cr_0 = sum(cl_res_0) / len(cl_res_0)
-av_cr_1 = sum(cl_res_1) / len(cl_res_1)
-
-print('')
-print('[Train Test] Average class for zeros:', av_cr_0)
-print('[Train Test] Average class for ones: ', av_cr_1)
-
-print('The result is more biased towards ' + ('zeros' if
-      (abs(av_cr_0 - class_0) < abs(av_cr_1 - class_1)) else 'ones'))
-
-mistakes_0 = sum([1 if cl_res_0[i] != class_0 else 0 for i in range(0, len(cl_res_0))])
-mistakes_1 = sum([1 if cl_res_1[i] != class_1 else 0 for i in range(0, len(cl_res_0))])
-print('[Train Test] Mistakes on zeros:', mistakes_0, '/', len(cl_res_0),
-      '(' + str(100 * mistakes_0 / len(cl_res_0)) + '%)')
-print('[Train Test] Mistakes on ones:', mistakes_1, '/', len(cl_res_1),
-      '(' + str(100 * mistakes_1 / len(cl_res_1)) + '%)')
-
-cl_res_0 = [em.classify(x) for x in test2[0]]
-cl_res_1 = [em.classify(x) for x in test2[1]]
-av_cr_0 = sum(cl_res_0) / len(cl_res_0)
-av_cr_1 = sum(cl_res_1) / len(cl_res_1)
-
-print('')
-print('[Testing] Average class for zeros:', av_cr_0)
-print('[Testing] Average class for ones: ', av_cr_1)
-
-print('The result is more biased towards ' + ('zeros' if
-      (abs(av_cr_0 - class_0) < abs(av_cr_1 - class_1)) else 'ones'))
-
-mistakes_0 = sum([1 if cl_res_0[i] != class_0 else 0 for i in range(0, len(cl_res_0))])
-mistakes_1 = sum([1 if cl_res_1[i] != class_1 else 0 for i in range(0, len(cl_res_0))])
-print('[Testing] Mistakes on zeros:', mistakes_0, '/', len(cl_res_0),
-      '(' + str(100 * mistakes_0 / len(cl_res_0)) + '%)')
-print('[Testing] Mistakes on ones:', mistakes_1, '/', len(cl_res_1),
-      '(' + str(100 * mistakes_1 / len(cl_res_1)) + '%)')
+analyze_set(train, 'Training')
+analyze_set(test1, 'Train Test')
+analyze_set(test2, 'Testing')
 
